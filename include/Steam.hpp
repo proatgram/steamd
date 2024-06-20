e0912f4f28d3960428cd7f8d49c2d0248618575e
Timothy Hutchins <tgh6407@gmail.com>
2024-06-19 21:07:54 -0500

    Started implementing DBus functions

--- include/Steam.hpp
+++ include/Steam.hpp
@@ -8,14 +8,31 @@ namespace steamd {
             ~Steamd();
 
         protected:
-            auto log_on_with_steamid(const uint64_t& steamId) -> void final;
+            auto log_on_with_steamid(const sdbus::Struct<uint32_t, uint32_t, int32_t, int32_t>& steamId) -> void final;
             auto log_on_with_credentials(const std::string& username, const std::string& password, const bool &rememberInfo) -> void final;
             auto try_cache_log_on(const std::string &username) -> void final;
+            auto log_on_with_two_factor(const std::string& username, const std::string& password, const bool &rememberInfo, const std::string& twoFactorCode) -> void final;
+            auto log_on_with_two_factor(const std::string& two_factor_code) -> void final;
+            auto log_on_with_steam_guard_code(const std::string& username, const std::string& password, const bool &rememberInfo, const bool &rememberComputer, const std::string& steamGuardCode) -> void final;
+            auto log_on_with_steam_guard_code(const bool &rememberComputer, const std::string& steamGuardCode) -> void final;
+            auto send_validation_email() -> void final;
             auto log_off() -> void final;
             auto get_log_on_state() -> int32_t final;
+            auto has_two_factor() -> bool final;
             auto is_logged_on() -> bool final;
             auto is_connected() -> bool final;
             auto is_trying_to_log_on() -> bool final;
+            auto has_cached_credentials(const std::string &username) -> bool final;
+            auto destroy_cached_credentials(const std::string &username) -> int32_t final;
+            auto is_password_remembered() -> bool final;
+            auto get_account_security_policy_flags() -> uint32_t final;
+            auto get_client_instance() -> uint32_t final;
+            auto is_VAC_banned(const uint32_t &appId) -> bool final;
+            auto is_behind_nat() -> bool final;
+            auto get_email() -> std::tuple<bool, bool, std::string> final;
+            auto set_email(const std::string &email) -> bool final;
+            auto get_user_base_folder() -> std::string final;
+            auto get_user_data_folder(const sdbus::Struct<uint32_t, uint32_t, uint32_t>& gameId) -> std::tuple<bool, std::string> final;
 
         private:
             STEAM_CALLBACK(Steamd, OnSteamServerConnectionFailure, SteamServerConnectFailure_t, m_cbSteamServerConnectFailure);
--- src/Steam.cpp
+++ src/Steam.cpp
@@ -1,6 +1,12 @@
 #include "Steam.hpp"
 
+#include <climits>
+
 using namespace steamd;
+
+/*
+ * All DBus structs are documented in the DBus XML stub
+ */
 
 Steamd::Steamd(sdbus::IConnection &dbusConnection, sdbus::ObjectPath objectPath) :
     AdaptorInterfaces(dbusConnection, std::move(objectPath)),
@@ -32,8 +38,9 @@ Steamd::~Steamd() {
     m_callbacksRunner.join();
 }
 
-auto Steamd::log_on_with_steamid(const uint64_t &steamId) -> void {
-    m_steamClientUser->LogOn(steamId);
+auto Steamd::log_on_with_steamid(const sdbus::Struct<uint32_t, uint32_t, int32_t, int32_t>& steamId) -> void {
+    CSteamID id = CSteamID(steamId.get<0>(), steamId.get<1>(), static_cast<EUniverse>(steamId.get<3>()), static_cast<EAccountType>(steamId.get<2>()));
+    m_steamClientUser->LogOn(id);
 }
 
 auto Steamd::log_on_with_credentials(const std::string &username, const std::string &password, const bool &rememberInfo) -> void {
@@ -41,17 +48,47 @@ auto Steamd::log_on_with_credentials(const std::string &username, const std::str
     m_steamClientUser->LogOn(m_steamClientUser->GetSteamID());
 }
 
+auto Steamd::log_on_with_two_factor(const std::string& username, const std::string& password, const bool &rememberInfo, const std::string& twoFactorCode) -> void {
+    m_steamClientUser->SetLoginInformation(username.c_str(), password.c_str(), rememberInfo);
+    m_steamClientUser->SetTwoFactorCode(twoFactorCode.c_str());
+    m_steamClientUser->LogOn(m_steamClientUser->GetSteamID());
+}
+
+auto Steamd::log_on_with_two_factor(const std::string &twoFactorCode) -> void {
+    m_steamClientUser->SetTwoFactorCode(twoFactorCode.c_str());
+    m_steamClientUser->LogOn(m_steamClientUser->GetSteamID());
+}
+
+auto Steamd::log_on_with_steam_guard_code(const std::string& username, const std::string& password, const bool &rememberInfo, const bool &rememberComputer, const std::string& steamGuardCode) -> void {
+    m_steamClientUser->SetLoginInformation(username.c_str(), password.c_str(), rememberInfo);
+    m_steamClientUser->Set2ndFactorAuthCode(steamGuardCode.c_str(), !rememberComputer);
+    m_steamClientUser->LogOn(m_steamClientUser->GetSteamID());
+}
+
+auto Steamd::log_on_with_steam_guard_code(const bool &rememberComputer, const std::string &steamGuardCode) -> void {
+    m_steamClientUser->Set2ndFactorAuthCode(steamGuardCode.c_str(), !rememberComputer);
+    m_steamClientUser->LogOn(m_steamClientUser->GetSteamID());
+}
+
+auto Steamd::send_validation_email() -> void {
+    m_steamClientUser->SendValidationEmail();
+}
+
 auto Steamd::try_cache_log_on(const std::string &username) -> void {
     m_steamClientUser->SetAccountNameForCachedCredentialLogin(username.c_str(), false);
     m_steamClientUser->LogOn(m_steamClientUser->GetSteamID());
+}
+
+auto Steamd::log_off() -> void {
+    m_steamClientUser->LogOff();
 }
 
 auto Steamd::get_log_on_state() -> int32_t {
     return static_cast<int32_t>(m_steamClientUser->GetLogonState());
 }
 
-auto Steamd::log_off() -> void {
-    m_steamClientUser->LogOff();
+auto Steamd::has_two_factor() -> bool {
+    return m_steamClientUser->BHasTwoFactor();
 }
 
 auto Steamd::is_logged_on() -> bool {
@@ -64,6 +101,72 @@ auto Steamd::is_connected() -> bool {
 
 auto Steamd::is_trying_to_log_on() -> bool {
     return m_steamClientUser->BTryingToLogin();
+}
+
+auto Steamd::has_cached_credentials(const std::string &username) -> bool {
+    return m_steamClientUser->BHasCachedCredentials(username.c_str());
+}
+
+auto Steamd::destroy_cached_credentials(const std::string &username) -> int32_t {
+    return m_steamClientUser->DestroyCachedCredentials(username.c_str());
+}
+
+auto Steamd::is_password_remembered() -> bool {
+    return m_steamClientUser->IsPasswordRemembered();
+}
+
+auto Steamd::get_account_security_policy_flags() -> uint32_t {
+    return m_steamClientUser->GetAccountSecurityPolicyFlags();
+}
+
+auto Steamd::get_client_instance() -> uint32_t {
+    return m_steamClientUser->GetClientInstanceID();
+}
+
+auto Steamd::is_VAC_banned(const uint32_t &appId) -> bool {
+    return m_steamClientUser->IsVACBanned(appId);
+}
+
+auto Steamd::is_behind_nat() -> bool {
+    return m_steamClientUser->BIsBehindNAT();
+}
+
+auto Steamd::get_email() -> std::tuple<bool, bool, std::string> {
+    std::string email = std::string(320, ' ');
+    bool validated{};
+    bool result = m_steamClientUser->GetEmail(email.data(), 320, &validated);
+    std::size_t index = email.find_first_of(' ');
+    if (index != std::string::npos) {
+        email.resize(index - 1);
+    }
+
+    return std::make_tuple(result, validated, email);
+}
+
+auto Steamd::set_email(const std::string &email) -> bool {
+    return m_steamClientUser->SetEmail(email.c_str());
+}
+
+auto Steamd::get_user_base_folder() -> std::string {
+    return m_steamClientUser->GetUserBaseFolder();
+}
+
+auto Steamd::get_user_data_folder(const sdbus::Struct<uint32_t, uint32_t, uint32_t>& gameId) -> std::tuple<bool, std::string> {
+    CGameID game = CGameID(gameId.get<0>());
+
+    if(gameId.get<1>() != 0) {
+        game = CGameID(gameId.get<0>(), gameId.get<2>());
+    }
+
+    std::string path = std::string(PATH_MAX, ' ');
+
+    bool result = m_steamClientUser->GetUserDataFolder(game, path.data(), PATH_MAX);
+    std::size_t index = path.find_first_of(' ');
+    if (index != std::string::npos) {
+        path.resize(index - 1);
+    }
+
+    return std::make_tuple(result, path);
 }
 
 /* Steam Callbacks */
